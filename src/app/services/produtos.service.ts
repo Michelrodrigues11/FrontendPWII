9a3735ae20af2e8e9aa970117b44123660faea66
Michel <henriquemichel10@gmail.com>
2023-11-09 22:22:08 -0300

    Foi

---
+++ src/app/services/produtos.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProdutosService {
+  private URL: string = 'http://localhost:3000';
+  constructor() { }
+
+  buscarTodos(){
+    
+  }
+}
